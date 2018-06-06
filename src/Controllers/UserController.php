5d3742e92e55e5fdefe25352eb2e0474b6899835
eyrad4 <eyrad4@gmail.com>
2018-06-06 12:04:20 +0300

    add login method

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace Mindk\Framework\Controllers;
 use Mindk\Framework\Exceptions\AuthRequiredException;
 use Mindk\Framework\Http\Request\Request;
 use Mindk\Framework\Models\UserModel;
+use Mindk\Framework\Models\RoleModel;
 use Mindk\Framework\DB\DBOConnectorInterface;
 
 /**
@@ -44,24 +45,30 @@ class UserController
      * @return mixed
      * @throws AuthRequiredException
      */
-    public function login(Request $request, UserModel $model, DBOConnectorInterface $dbo) {
+    public function login(Request $request, UserModel $model, RoleModel $roleModel, DBOConnectorInterface $dbo) {
 
-        if($login = $request->get('login', '', 'string')) {
+        if( !empty($request->get('login', '', 'string'))
+            AND !empty($request->get('password', '', 'string'))) {
+            if($login = $request->get('login', '', 'string')) {
+                $user = $model->findByCredentials($login, $request->get('password', ''));
+            }
+            if (empty($user)) {
+                throw new AuthRequiredException('Bad access credentials provided');
+            }
+            // Generate new access token and save:
+            $user->token = md5(uniqid());
+            //$user->tokenExpire = strtotime("+1 day");
+            $params = [
+                'token' => $user->token,
+                //'token_expire' => $user->tokenExpire
+            ];
 
-            $user = $model->findByCredentials($login, $request->get('password', ''));
+            $model->update($params, '`id`=' . (int)$user->id . '');
+            return ['token' => $user->token, 'userId' => $user->id, 'login' => $login, 'userRole' => $roleModel->findByRoleId($user->role)];
+        }else{
+            throw new AuthRequiredException('Not all required fields are filled in');
         }
 
-        if(empty($user)) {
-            throw new AuthRequiredException('Bad access credentials provided');
-        }
-
-        // Generate new access token and save:
-        $user->token = md5(uniqid());
-        //$user->save();
-        //@TODO: REMOVE THIS when UserModel::save() implemented
-        $dbo->setQuery("UPDATE `users` SET `token`='".$user->token."' WHERE `id`=".(int)$user->id);
-
-        return $user->token;
     }
 
     public function logout(Request $request) {
