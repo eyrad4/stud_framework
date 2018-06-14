1d421b6aec168a587cd0f88412e7f0e00d7e2dce
eyrad4 <eyrad4@gmail.com>
2018-06-14 18:42:48 +0300

    add prepare methods for parse msql where, order by limit, add method save and delete update and insert

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -35,6 +35,65 @@ abstract class Model
     }
 
     /**
+     * @param string|array $data
+     * @param bool $ignoreAlias
+     * @return string
+     */
+    protected function prepareFields($data){
+        if (\is_array($data)) {
+            $tmp = [];
+            foreach ($data as $alias => $field) {
+                $tmp[] = ($alias !== $field && ! \is_int($alias)) ?
+                    ($field . ' as `' . $alias . '`') : $field;
+            }
+            $data = implode(',', $tmp);
+        }
+        if (empty($data)) {
+            $data = '*';
+        }
+        return $data;
+    }
+
+    /**
+     * @param array|string $data
+     * @return string
+     */
+    protected function prepareWhere($data) {
+        if (\is_array($data)) {
+            if ($this->arrayOnlyNumeric(array_keys($data)) === true) {
+                $data = implode(' ', $data);
+            }
+        }
+        $data = trim($data);
+        if (!empty($data) && stripos($data, 'WHERE') !== 0) {
+            $data = "WHERE {$data}";
+        }
+        return $data;
+    }
+    /**
+     * @param string $data
+     * @return string
+     */
+    protected function prepareOrder($data) {
+        $data = trim($data);
+        if (! empty($data) && stripos($data, 'ORDER') !== 0) {
+            $data = "ORDER BY {$data}";
+        }
+        return $data;
+    }
+    /**
+     * @param string $data
+     * @return string
+     */
+    protected function prepareLimit($data) {
+        $data = trim($data);
+        if (! empty($data) && stripos($data, 'LIMIT') !== 0) {
+            $data = "LIMIT {$data}";
+        }
+        return $data;
+    }
+
+    /**
      * Create new record
      */
     public function create( $data ) {
@@ -49,43 +108,30 @@ abstract class Model
      * @return  object
      */
     public function load( $id ) {
-        $sql = 'SELECT * FROM `' . $this->tableName .' ` WHERE `'.$this->primaryKey.'`='.(int)$id; //!
+        $sql = 'SELECT * FROM `' . $this->tableName .'` WHERE `'.$this->primaryKey.'`='.(int)$id; //!
 
         return $this->dbo->setQuery($sql)->getResult($this);
     }
 
     /**
-     * Save record state to db
-     *
-     * @return bool
+     * @param string|array $fields
+     * @param string $table
+     * @param array|string $where
+     * @return mixed
      */
-    public function save($params) {
-        $sql = 'INSERT INTO `'.$this->tableName.'`
-        (`'.implode('`, `', array_keys($params)).'`)
-        VALUES("'.implode('", "', array_values($params)).'")';
-
-        return $this->dbo->setQuery($sql);
-    }
-
-    /**
-     * Update record state to db
-     *
-     * @return bool
-     */
-    public function update($params, $where) {
-
-        if(!empty($params)){
-            $updateParams = '';
-            foreach($params as $name => $param){
-                $updateParams .= '`'.$name.'` = "'.$param.'",';
+    public function save($params, $where = '')
+    {
+        if ($where === '') {
+            $mode = 'insert';
+        } else {
+            if (empty($this->select('*', $where))) {
+                $mode = 'insert';
+            } else {
+                $mode = 'update';
             }
-
-            $sql = 'UPDATE  `'.$this->tableName.'` SET
-            '.rtrim($updateParams, ',').'
-            WHERE '.$where.' ';
-
-            return $this->dbo->setQuery($sql);
         }
+
+        return ($mode === 'insert') ? $this->insert($params) : $this->update($params, $where);
     }
 
     /**
@@ -103,7 +149,24 @@ abstract class Model
      * @return array
      */
     public function getList() {
-        $sql = 'SELECT * FROM `' . $this->tableName . '`';
+        $sql = 'SELECT * FROM `' . $this->tableName . '` ';
+
+        return $this->dbo->setQuery($sql)->getList(get_class($this));
+    }
+
+    /**
+     * Get list of records witch same sql commands
+     *
+     * @return array
+     */
+    public function select($fields = '*', $where = '', $orderBy = '', $limit = ''){
+
+        $fields = $this->prepareFields($fields);
+        $where = $this->prepareWhere($where);
+        $orderBy = $this->prepareOrder($orderBy);
+        $limit = $this->prepareLimit($limit);
+
+        $sql = 'SELECT '.$fields.' FROM `' . $this->tableName . '` '.$where.' '.$orderBy.' '.$limit.' ';
 
         return $this->dbo->setQuery($sql)->getList(get_class($this));
     }
